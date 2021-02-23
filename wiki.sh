ad912f83d9ec5ce1c6024dd13220f6cffaa2871a
Justin Lim <jlim0930@yahoo.com>
2021-02-23 16:09:42 -0600

    updated wiki.sh

--- wiki.sh
+++ wiki.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# this script will use the linuxserver's dokuwiki container and manage your content on a github repo
+# This script will use linuxserver.io dokuwiki container and manage your content on github repo
 
-# Please create a github repo named wiki and set it to private.
-# setup ssh keys for github for this script to work correctly
+# Please create a github repo named wiki and set it to private
+# setup ssh keys for github so that this script can use it to get/push content
 
 
-# Assumptions:
-# 1 the wiki will be placed in your homedir/wiki
-# 2 your container name will be wiki
+# This script will create ${HOME}/wiki to store your content
+# Container name will be wiki
 
-# Please edit vars below
-GHUSERNAME="jlim0930"
+# Editable variables
+GHUSERNAME="<GITHUB USERNAME>"
 PORT="9090"
+TLSPORT="9091"
 
 # colors
 red=`tput setaf 1`
@@ -25,98 +25,109 @@ groupID=$(id -g)
 DATE=$(date +"%Y%m%d-%H%M")
 
 # functions
-bootstrap () {
-    if [ ! -d ~/wiki ]; then
-        echo "${green}[DEBUG]${reset} Creating ~/wiki"
-        mkdir ~/wiki
-        git clone git@github.com:${GHUSERNAME}/wiki.git ~/wiki
-        if [ $? -ne 0 ]; then
-          echo "${red}[DEBUG]${reset} Unable to clone from github. Exiting..."
-          exit
-        fi
-
+build() {
+  #
+  if [ ! -d ${HOME}/wiki ]; then
+    echo "${green}[DEBUG]${reset} Creating ${HOME}/wiki"
+    mkdir -p ${HOME}/wiki > /dev/null 2>&1
+    git clone git@github.com:${GHUSERNAME}/wiki.git ${HOME}/wiki
+    if [ $? -ne 0 ]; then
+      echo "${red}[DEBUG]${reset} Unable to clone from github.  Exiting...."
+      exit
     fi
+# decided this isnt needed
+#  else
+#    echo "${green}[DEBUG]${reset} ${HOME}/wiki already exists.. will create the container"
+  fi
+  if [ $(docker ps | grep -c wiki) -ge 1 ]; then
+    echo "${green}[DEBUG]${reset} wiki container is already running"
+  elif [ $(docker ps -a | grep -c wiki) -ge 1 ]; then
+    echo "${green}[DEBUG]${reset} wiki container exits but is not running. Starting container"
+    docker start wiki >/dev/null 2>&1
+  elif [ $(docker ps -a | grep -c wiki) -eq 0 ]; then
+    echo "${green}[DEBUG]${reset} Creating wiki container"
+    docker run -d --name=wiki -e PUID=${userID} -e GUID=${groupID} -e TZ=America\Chicago -p ${PORT}:80 -p ${TLSPORT}:443 -v ${HOME}/wiki:/config --restart unless-stopped ghcr.io/linuxserver/dokuwiki >/dev/null 2>&1
+  fi
+}
+
+stopcontainer() {
+  if [ $(docker ps -a | grep -c wiki) -ge 1 ]; then
+    echo "${green}[DEBUG]${reset} Stopping wiki container"
+    docker stop wiki >/dev/null 2>&1
+  else
+    echo "${red}[DEBUG]${reset} container is not running.  Nothing to stop"
+  fi
 }
 
 gitpush () {
-    if [ ! -d ~/wiki ]; then
-        echo "${red}[DEBUG]${reset} ~/wiki does not exist so nothing to push."
-        exit
-    fi
-    cd ~/wiki
-    if [ ! -f ~/wiki/.gitignore ]; then
-      cat > ~/wiki/.gitignore<<EOF
+  if [ ! -d ${HOME}/wiki ]; then
+    echo "${red}[DEBUG]${reset} ${HOME}/wiki does not exist so nothing to push."
+    exit
+  fi
+  cd ${HOME}/wiki
+  if [ ! -f ${HOME}/wiki/.gitignore ]; then
+    cat > ${HOME}/wiki/.gitignore<<EOF
 log/*/*.log
 EOF
-    fi
-    git add .
-    git commit -m "wiki updated ${DATE}"
-    git branch -M main
-    git push -u origin main
-    if [ $? -ne 0 ]; then
-      echo "${red}[DEBUG]${reset} Unable to push to github"
-    fi
-}
+  fi
+  git add .
+  git commit -m "wiki updated ${DATE}"
+  git branch -M main
+  git push -u origin main
+  if [ $? -ne 0 ]; then
+    echo "${red}[DEBUG]${reset} Unable to push to github"
+  fi
+ }
 
 gitpull () {
-    cd ~/wiki
-    git reset --hard
-    git pull --rebase
-    if [ $? -ne 0 ]; then
-      echo "${red}[DEBUG]${reset} Unable to pull from github"
-    else
-      docker restart wiki
-    fi
-}
+  cd ${HOME}/wiki
+  git reset --hard
+  git pull --rebase
+  if [ $? -ne 0 ]; then
+    echo "${red}[DEBUG]${reset} Unable to pull from github"
+  else
+    docker restart wiki >/dev/null 2>&1
+  fi
+ }
 
-containerstart () {
-    if [ ! "$(docker ps -q -f name=wiki)" ]; then
-      if [ "$(docker ps -aq -f status=exited -f name=wiki)" ]; then
-        # cleanup
-        docker rm wiki
-      fi
-      # run your container
-      bootstrap
-      docker run -d --name=wiki -e PUID=${userID} -e GUID=${groupID} -e TZ=America\Chicago -p ${PORT}:80 -v ~/wiki:/config --restart unless-stopped ghcr.io/linuxserver/dokuwiki
-    fi
+cleanup() {
+  echo "${green}[DEBUG]${reset} Stopping container"
+  docker stop wiki >/dev/null 2>&1
+  echo "${green}[DEBUG]${reset} Removing container"
+  docker rm wiki >/dev/null 2>&1
+  echo "${green}[DEBUG]${reset} Removing ${HOME}/wiki directory"
+  rm -rf ${HOME}/wiki
 }
-
-cleanup () {
-    docker stop wiki
-    docker rm wiki
-    rm -rf ~/wiki
-}
-
 
 case $1 in
-    bootstrap)
-        bootstrap
-        ;;
-    push)
-        gitpush
-        ;;
-    pull)
-        gitpull
-        ;;
-    start)
-        containerstart
-        ;;
-    stop)
-        docker stop wiki
-        ;;
-    cleanup)
-        cleanup
-        ;;
-    *)
-        echo "${green}Usage:${reset} ./`basename $0` command"
-#        echo "                 ${green}bootstrap${reset} - Create ~/wiki and git clone"
-        echo "                 ${green}push${reset} - push contents to git repo"
-        echo "                 ${green}pull${reset} - pull contents from git repo"
-        echo "                 ${green}start${reset} - init and start container"
-        echo "                 ${green}stop${reset} - stops wiki container"
-        echo "                 ${green}cleanup${reset} - stops and removes container and ~/wiki"
-        echo ""
-        echo "Please make sure edit the editable section in the script"
-        echo "Please create your github repo first"
-        ;;
+  build)
+    build
+    ;;
+  start)
+    build
+    ;;
+  pull)
+    gitpull
+    ;;
+  push)
+    gitpush
+    ;;
+  stop)
+    stopcontainer
+    ;;
+  cleanup)
+    cleanup
+    ;;
+  *)
+    echo "${green}Usage:${reset} ./`basename $0` command"
+    echo "                 ${green}build${reset}   - Create ${HOME}/wiki and git clone"
+    echo "                 ${green}push${reset}    - push contents to git repo"
+    echo "                 ${green}pull${reset}    - pull contents from git repo"
+    echo "                 ${green}start${reset}   - init and start container"
+    echo "                 ${green}stop${reset}    - stops wiki container"
+    echo "                 ${green}cleanup${reset} - stops and removes container and ${HOME}/wiki"
+    echo ""
+    echo "Please make sure edit the editable section in the script"
+    echo "Please create your github repo first"
+    ;;
 esac
