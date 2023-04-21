2389144b443c96ef0caed97dfd5dd9498cdeec2e
Justin Lim <jlim0930@yahoo.com>
2023-04-21 15:02:45 -0500

    changed gcp.sh to add more than 1 instance

--- gcp.sh
+++ gcp.sh
@@ -1,16 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-## Creates a GCP instance
+## Creates GCP instances
 
-# ------- EDIT information below to customize for your needs
-gcp_name="justinlim-lab"        # name of the compute instance stood up
-
-##############################################################################
-
-gcp_project="elastic-support"   # project that you have access to to stand up the compute instance
+# --------------EDIT information below
+gcp_name="justinlim-lab"
+##############################
+gcp_project="elastic-support"
 gcp_zone="us-central1-a"        # GCP zone - select one that is close to you
 machine_type="e2-standard-4"    # GCP machine type - gcloud compute machine-types list
-#boot_disk_size="50"             # boot disk size
 boot_disk_type="pd-ssd"         # disk type -  gcloud compute disk-types list
 label="division=support,org=support,team=support,project=${gcp_name}"
 
@@ -30,44 +27,20 @@ help()
   echo "${blue}COMMANDS${reset}"
   echo "  ${green}create${reset} - Creates your GCP environment & run post-scripts(linux)"
   echo "           defaults to centos-7 if image is not specified"
-  echo "           ${blue}Popular supported IMAGES: ${reset}"
-  echo "                  centos-7 rhel-8 rocky-linux-8 ubuntu-2204-lts widnows-2022"
-  echo "           ${green}Example: ${blue} ./gcp.sh create rocky-linux-8${reset}"
+  echo "           defaults to 1 gcp instance if number of instance is not specified"
   echo "  ${green}list${reset} - List all available images"
   echo "  ${green}find${reset} - Finds info about your GCP environment"
   echo "  ${green}delete${reset} - Deletes your GCP environment"
 
 } # end help
 
-create() 
-{
-  find
-  echo "${green}[DEBUG]${reset} Creating instance ${gcp_name} with ${1}"
-  
-  find_image ${1}
-  
-  gcloud compute instances create ${gcp_name} \
-      --labels ${label} \
-      --project=${gcp_project} \
-      --zone=${gcp_zone} \
-      --machine-type=${machine_type} \
-      --network-interface=network-tier=PREMIUM,subnet=default \
-      --maintenance-policy=MIGRATE \
-      --provisioning-model=STANDARD \
-      --tags=http-server,https-server \
-      --create-disk=auto-delete=yes,boot=yes,device-name=${gcp_name},image=projects/${PROJECT}/global/images/${IMAGE},mode=rw,type=projects/elastic-support/zones/${gcp_zone}/diskTypes/${boot_disk_type} \
-      --metadata=startup-script='#!/usr/bin/env bash
-      if [ ! -f /ran_startup ]; then
-        curl -s https://raw.githubusercontent.com/jlim0930/scripts/master/gcp-postinstall.sh | bash
-      fi'
- 
-  echo ""
-  echo "${green}[DEBUG]${reset} Compute instance is being deployed  Please ${blue}gcloud compute ssh ${gcp_name} [--zone ${gcp_zone}]${reset}.  There is a post install script running and it will reboot the instance once complete, usually in about 3-5 minutes."
-  echo ""
-  echo "${green}[DEBUG]${reset} If you are running windows instance.  Please create your password ${blue}gcloud compute reset-windows-password ${gcp_name}${reset}"
+# image_list
+image_list() {
+  echo "${green}[DEBUG]${reset} Full list of supported images"
+  gcloud compute images list | grep -v arm | grep READY | grep "\-cloud " | awk {' print $3 '} | grep -v READY | sort -n
+}
 
-} # end start
-
+# find image
 find_image() {
   unset STRING
   unset PROJECT
@@ -85,11 +58,7 @@ find_image() {
   fi
 }
 
-image_list() {
-  echo "${green}[DEBUG]${reset} Full list of supported images"
-  gcloud compute images list | grep -v arm | grep READY | grep "\-cloud " | awk {' print $3 '} | grep -v READY | sort -n
-}
-
+# wait_vm_up
 wait_vm_up() {
   unset status
   while [ "${status}" != "done" ]
@@ -104,7 +73,7 @@ wait_vm_up() {
   exit
 }
 
-
+# find
 find() {
   # finds the info for your compute instance
   if [ $(gcloud compute instances list 2> /dev/null --project ${gcp_project} | grep ${gcp_name} | wc -l) -gt 0 ]; then
@@ -117,23 +86,69 @@ find() {
   fi
 }  # end find
 
+
+# create
+create() 
+{
+  find_image ${image}
+  find
+  
+  for count in $(seq 1 ${max})
+  do
+    echo "${green}[DEBUG]${reset} Creating instance ${blue}${gcp_name}-${count}${reset} with ${blue}${image}${reset} total of ${blue}${max}${reset} instances"
+    gcloud compute instances create ${gcp_name}-${count} \
+      --labels ${label} \
+      --project=${gcp_project} \
+      --zone=${gcp_zone} \
+      --machine-type=${machine_type} \
+      --network-interface=network-tier=PREMIUM,subnet=default \
+      --maintenance-policy=MIGRATE \
+      --provisioning-model=STANDARD \
+      --tags=http-server,https-server \
+      --create-disk=auto-delete=yes,boot=yes,device-name=${gcp_name}-${count},image=projects/${PROJECT}/global/images/${IMAGE},mode=rw,type=projects/elastic-support/zones/${gcp_zone}/diskTypes/${boot_disk_type} \
+      --metadata=startup-script='#!/usr/bin/env bash
+      if [ ! -f /ran_startup ]; then
+        curl -s https://raw.githubusercontent.com/jlim0930/scripts/master/gcp-postinstall.sh | bash
+      fi'
+ 
+    echo ""
+  done
+  echo "${green}[DEBUG]${reset} Compute instance is being deployed  Please ${blue}gcloud compute ssh ${gcp_name}-X [--zone ${gcp_zone}]${reset}.  There is a post install script running and it will reboot the instance once complete, usually in about 3-5 minutes."
+  echo ""
+  echo "${green}[DEBUG]${reset} If you are running windows instance.  Please create your password ${blue}gcloud compute reset-windows-password ${gcp_name}${reset}"
+  sleep 5
+  find
+} # end create
+
 delete() {
   if [ $(gcloud compute instances list 2> /dev/null --project ${gcp_project} | grep ${gcp_name} | wc -l) -gt 0 ]; then
-    echo "${green}[DEBUG]${reset} Deleting ${gcp_name}"
-    gcloud compute instances delete ${gcp_name} --project ${gcp_project} --zone=${gcp_zone} --quiet;
-
+    for instance in $(gcloud compute instances list | grep ${gcp_name} | awk {' print $1 '})
+      do
+        echo "${green}[DEBUG]${reset} Deleting ${instance}"
+        gcloud compute instances delete ${instance} --project ${gcp_project} --zone=${gcp_zone} --quiet;
+      done
   else 
     echo "${red}[DEBUG]${reset} Instance ${gcp_name} not found"
   fi
 } # end delete
 
+## main body
 case ${1} in
   create|start)
-    if [ ! -z "${2}" ]; then
-      create "${2}"
-    else
-      create "centos-7"
+    # Get image
+    image_list
+    read -p "Please select an image [centos-7] : " image
+    echo ""
+    if [ -z ${image} ]; then
+      image="centos-7"
     fi
+    # Get # of instances
+    read -p "Please input number of instances [1] : " max
+    echo ""
+    if [ -z ${max} ]; then
+      max="1"
+    fi
+    create ${image}
 #    wait_vm_up
     ;;
   find|info|status|check)
