5f94671c5b8ac9f7c7b1c03b9fb96bcc4deeb523
Justin Lim <jlim0930@yahoo.com>
2023-08-29 21:55:32 -0500

    added gcp-postinstall.ps1

---
+++ gcp-postinstall.ps1
@@ -0,0 +1,15 @@
+# execution policy
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+
+# fix for ssl
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+# install scoop
+irm get.scoop.sh | iex
+# iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
+scoop install sudo
+
+scoop install 7zip git
+scoop bucket add extras
+
+scoop install putty firefox mobaxterm 7zip powertoys sysinternals
