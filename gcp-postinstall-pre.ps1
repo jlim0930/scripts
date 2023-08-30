c585b4a2b290577861c70e9c559157d6692052e0
Justin Lim <jlim0930@yahoo.com>
2023-08-30 08:36:14 -0500

    added gcp-postinstall-pre.ps1

---
+++ gcp-postinstall-pre.ps1
@@ -0,0 +1,6 @@
+$text = @"
+@echo off
+powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "& {iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/jlim0930/scripts/master/gcp-postinstall.ps1'))}"
+"@
+
+Add-Content -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat" -Value $text -NoNewline -Force
